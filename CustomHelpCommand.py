334a984f072c6dc57a16113139c6c47281a0cb35
Noustaa <noustaa@gmail.com>
2022-06-22 23:12:25 +0200

    Few modifications to respect PEP8

--- CustomHelpCommand.py
+++ CustomHelpCommand.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 
+
 # class that defines the custom help command
 class CustomHelpCommand(commands.HelpCommand):
     def __init__(self):
@@ -8,12 +9,14 @@ class CustomHelpCommand(commands.HelpCommand):
     # method that is called when executing !help, it will list all available commands and their descriptions from cogs
     async def send_bot_help(self, mapping):
         for cog in mapping:
-            await self.get_destination().send(f'{cog.qualified_name}: {[command.name for command in mapping[cog]]} - {[command.description for command in mapping[cog]]}')
+            await self.get_destination().send(f'{cog.qualified_name}: {[command.name for command in mapping[cog]]} - '
+                                              f'{[command.description for command in mapping[cog]]}')
 
     # method that is called when executing !help <cog_name>, it will list all available commands in a specific cog
     # and their descriptions
     async def send_cog_help(self, cog):
-        await self.get_destination().send(f'{cog.qualified_name}: {[command.name for command in cog.get_commands()]} - {[command.description for command in cog.get_commands()]}')
+        await self.get_destination().send(f'{cog.qualified_name}: {[command.name for command in cog.get_commands()]} -'
+                                          f' {[command.description for command in cog.get_commands()]}')
 
     # method that is called when executing !help <command_name>, it will display the specific command description
     async def send_command_help(self, command):
